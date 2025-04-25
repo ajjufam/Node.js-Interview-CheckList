704c9e626bee9a624c4572ba40d9990ba458eb3d
ajjufam <au.ajju7@outlook.com>
2025-04-25 21:54:10 +0530

    concat

---
+++ 4. Array Methods/b7.concat.js
@@ -0,0 +1,15 @@
+// concat()
+//     The concat() method in JavaScript is used to combine two or more arrays into a new array.
+//     It does not change the original arrays.
+
+//     Why ?
+//     Use it when you want to merge arrays without modifying the originals.
+
+let arr1 = [1, 2, 3];
+let arr2 = [4, 5, 6];
+
+const concat = arr1.concat(arr2);
+console.log(concat); // [ 1, 2, 3, 4, 5, 6 ]
+
+console.log(arr1); // [ 1, 2, 3 ]
+console.log(arr2); // [ 4, 5, 6 ]
