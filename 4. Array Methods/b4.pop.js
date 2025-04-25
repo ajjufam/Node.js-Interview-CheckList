a7fff97fdc2b0c57a6459447ac30789e31c7fea9
ajjufam <au.ajju7@outlook.com>
2025-04-25 21:31:17 +0530

    pop

---
+++ 4. Array Methods/b4.pop.js
@@ -0,0 +1,12 @@
+// pop()
+//     The pop() method in JavaScript removes the last element from an array and returns
+//     that removed element.
+
+//     Why ?
+//     Use it when you want to remove the last item from an array.
+
+let arr = [1, 2, 3, 4, 5];
+let popped = arr.pop();
+
+console.log(popped); // 5
+console.log(arr); // [1,2,3,4]
