290702aa18cc65c0ddf11fee677c14e3bdc5499a
ajjufam <au.ajju7@outlook.com>
2025-04-25 23:18:10 +0530

    reverse

---
+++ 4. Array Methods/c3.reverse.js
@@ -0,0 +1,12 @@
+// reverse()
+//     The reverse() method in JavaScript reverses the order of elements in an
+//     array in place (modifies the original array).
+
+//     Why ?
+//     Use it when you want to flip the order of items in an array.
+
+const nums = [4, 3, 2, 1];
+
+let reversed = nums.reverse();
+console.log(reversed); // [1,2,3,4]
+console.log(nums); // [1,2,3,4] (changes the original array)
