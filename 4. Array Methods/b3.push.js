1536891b4af1ca0f90317aa885a4bedb29987c53
ajjufam <au.ajju7@outlook.com>
2025-04-25 21:26:25 +0530

    push

---
+++ 4. Array Methods/b3.push.js
@@ -0,0 +1,12 @@
+// push()
+//     The push() method in JavaScript adds one or more elements to the end of an array and returns
+//     the new length of the array.
+
+//     Why ?
+//     Use it when you want to add items to the end of an array.
+
+let arr = [1, 2, 3];
+let newLength = arr.push(4, 5);
+
+console.log(arr); // [1,2,3,4,5]
+console.log(newLength); // 5 (length)
