2cb5e12e6e0df6fd8c69aaffcb6475b3adef8674
ajjufam <au.ajju7@outlook.com>
2025-04-25 21:16:12 +0530

    indexOf

---
+++ 4. Array Methods/b1.indexOf.js
@@ -0,0 +1,10 @@
+// indexOf()
+//     The indexOf() method in JavaScript returns the index of the first occurrence of a specified value in an array.
+//     If the value is not found, it returns -1.
+
+//     Why ?
+//     Use it to find where a specific value is located in an array.
+
+let arr = ["mango", "apple", "banana"];
+const result = arr.indexOf("banana");
+console.log(result); // 2
