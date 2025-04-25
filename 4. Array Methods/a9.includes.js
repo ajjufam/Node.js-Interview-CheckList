5803e08325ed9f1d59e3a4793ecbefc41b4f388f
ajjufam <au.ajju7@outlook.com>
2025-04-25 21:09:03 +0530

    includes

---
+++ 4. Array Methods/a9.includes.js
@@ -0,0 +1,10 @@
+// includes()
+//     The includes() method in JavaScript checks if an array contains a specific value.
+//     It returns true if found, otherwise false.
+
+//     Why ?
+//     Use it when you simply want to check if a value exists in an array.
+
+let arr = ["banana", "mango", "apple"];
+let result = arr.includes("banana");
+console.log(result); // true
