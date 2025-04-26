cf7b60cfe6f00766ec93973faf54c386d83a561f
ajjufam <au.ajju7@outlook.com>
2025-04-27 01:15:19 +0530

    charCodeAt

---
+++ 5. String Methods/a8.charCodeAt.js
@@ -0,0 +1,10 @@
+// charCodeAt()
+//     charCodeAt() in JavaScript returns the Unicode number (ASCII code) of the character at a specific index.
+
+//     Why?
+//     Use it when you need to know the Unicode/ASCII value of a character.
+
+let str = "char code A";
+console.log(str.charCodeAt(2)); // char Code of 'a' : 97
+console.log(str.charCodeAt(3)); // char Code of 'r' : 114
+console.log(str.charCodeAt(10)); // char Code of 'A' : 65
