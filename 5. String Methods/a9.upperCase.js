40096aada51e18cc5a421d7579f1f08e487e5339
ajjufam <au.ajju7@outlook.com>
2025-04-27 01:19:17 +0530

    upperCase

---
+++ 5. String Methods/a9.upperCase.js
@@ -0,0 +1,9 @@
+// toUpperCase()
+//     toUpperCase() in JavaScript converts all letters in a string to uppercase (capital letters).
+
+//     Why?
+//     Use it when you want to make text all capital — for display, comparison, etc.
+
+let str = "hello prajwal";
+let upper = str.toUpperCase();
+console.log(upper); // HELLO PRAJWAL
