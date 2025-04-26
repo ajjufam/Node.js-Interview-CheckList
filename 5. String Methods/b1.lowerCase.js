7c6827ecf0eeea7e4cdb0856ee3ddde85f783268
ajjufam <au.ajju7@outlook.com>
2025-04-27 01:21:26 +0530

    lowerCase

---
+++ 5. String Methods/b1.lowerCase.js
@@ -0,0 +1,8 @@
+// toLowerCase()
+//     toLowerCase() in JavaScript converts all letters in a string to lowercase (small letters).
+
+//     Why?
+//     Use it when you want to make text all small — for display, comparison, or formatting.
+
+let str = "HELLO AJJU";
+console.log(str.toLowerCase()); // hello ajju
