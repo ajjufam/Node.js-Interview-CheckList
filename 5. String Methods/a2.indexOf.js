5bf258a7be793e6380fef786c343fc3a75cc04f7
ajjufam <au.ajju7@outlook.com>
2025-04-26 15:09:35 +0530

    indexOf

---
+++ 5. String Methods/a2.indexOf.js
@@ -0,0 +1,10 @@
+// indexOf()
+//     indexOf() finds the first occurrence of a value in a string or array and returns its index.
+//     If not found, it returns -1.
+
+//     Why ?
+//     Use it when you want to know where something appears (its position) inside an array or string.
+
+let str = "hello hello";
+let indexOf = str.indexOf("o"); // first index of 'o'
+console.log(indexOf);
