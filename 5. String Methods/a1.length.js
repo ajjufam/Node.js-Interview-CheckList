3bfc04c67a0b155459aa7359c9a2060c1b77d3be
ajjufam <au.ajju7@outlook.com>
2025-04-26 15:05:13 +0530

    length

---
+++ 5. String Methods/a1.length.js
@@ -0,0 +1,10 @@
+// length
+//     In JavaScript, .length on a string tells you how many characters are in that string.
+
+//     Why ?
+//     Use it when you want to know the size of a string.
+
+let str = "ajju";
+const len = str.length;
+
+console.log(len); // 4
