56f19bf0c6db2566f6a5c9f3708f6877c5a5601e
ajjufam <au.ajju7@outlook.com>
2025-04-27 01:54:49 +0530

    trimEnd

---
+++ 5. String Methods/c2.trimEnd.js
@@ -0,0 +1,10 @@
+// trimEnd()
+//     trimEnd() in JavaScript removes whitespace from the end of a string.
+
+//     Why?
+//     Use it when you want to clean up a string by removing spaces, tabs, or newlines from the end of the string,
+//     but keep the spaces at the beginning intact.
+
+const message = "   Hello World!   ";
+
+console.log(message.trimEnd()); // "   Hello World!"
