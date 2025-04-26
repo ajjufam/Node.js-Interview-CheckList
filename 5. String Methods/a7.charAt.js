732eea5d231c5cc88f984340c4d4de17cc520d69
ajjufam <au.ajju7@outlook.com>
2025-04-27 01:11:52 +0530

    charAt

---
+++ 5. String Methods/a7.charAt.js
@@ -0,0 +1,8 @@
+// charAt()
+//     charAt() in JavaScript returns the character at a specific index in a string.
+
+//     Why?
+//     Use it when you want to get a single character from a string.
+
+let str = "Ajnu";
+console.log(str.charAt(2)); // "n"
