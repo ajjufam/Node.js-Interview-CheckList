e18472658e204dd37d44d74434876072dec56e81
ajjufam <au.ajju7@outlook.com>
2025-04-27 13:28:35 +0530

    objects

---
+++ 7. Data Structures/a2.objects.js
@@ -0,0 +1,19 @@
+// Object
+//     An Object in JavaScript is a collection of key-value pairs.
+//     Key: A string (can also be a symbol).
+//     Value: Can be any data type (string, number, array, another object, etc.).
+
+//     Why?
+//     Objects help you store and organize related data in a single structure.
+
+// Ex
+let person = {
+  name: "Alice", // Key: "name", Value: "Alice"
+  age: 30, // Key: "age", Value: 30
+  city: "Wonderland", // Key: "city", Value: "Wonderland"
+};
+
+console.log(person.name); // Alice
+console.log(person.age); // 30
+
+// for more info check 6. Object Handling
