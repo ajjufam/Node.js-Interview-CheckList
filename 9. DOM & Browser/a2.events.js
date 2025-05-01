0d5d0bbc705b4b96480842efed8907a318cb425a
ajjufam <au.ajju7@outlook.com>
2025-05-01 06:21:46 +0530

    events

---
+++ 9. DOM & Browser/a2.events.js
@@ -0,0 +1,24 @@
+// Events
+//     Events are actions that happen in the browser, like:
+//     * A user clicking a button
+//     * Typing in an input field
+//     * Page loading
+//     * Mouse hovering over an element
+//     * Scrolling, etc.
+
+//     Why?
+//     They allow JavaScript to respond to user interactions dynamically.
+
+//     How?
+//     Using event listeners.
+
+// Example (Browser JavaScript):
+{
+  /* <button id="btn">Click me</button>
+
+<script>
+  document.getElementById("btn").addEventListener("click", function () {
+    alert("Button was clicked!");
+  });
+</script> */
+}
